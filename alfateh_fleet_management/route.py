0e95bfa73d37902573409cf9f12854d5143cea76
umair-gujjar <engrumair_sabir@yahoo.com>
2016-02-04 19:21:58 +0500

    modify hour field and add name field

--- alfateh_fleet_management/route.py
+++ alfateh_fleet_management/route.py
@@ -6,6 +6,7 @@ from openerp import models, fields, api
 class route_management(models.Model):
 	_name = 'route.management'
 	_inherit = ['mail.thread', 'ir.needaction_mixin']
+	name = fields.Char('Name')
 	route_defination = fields.Char('Route Defination')
 	route_time = fields.Float('Time(hours)')
 	route_distance = fields.Float('Distance(km)')
