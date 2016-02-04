4180fb1512649c4b41202178ad9784b149351aa7
umair-gujjar <engrumair_sabir@yahoo.com>
2016-02-04 18:20:19 +0500

    add route model and view

---
+++ alfateh_fleet_management/route.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+# Route Management
+class route_management(models.Model):
+	_name = 'route.management'
+	route_defination = fields.Char('Route Defination')
+	route_time = fields.Float('Time(hours)')
+	route_distance = fields.Float('Distance(km)')
+	route_fuel = fields.Float('Fuel(liters)')
+
+
