7c8998fc4751c5959ce0ed801d2ef779fc919fde
umair-gujjar <engrumair_sabir@yahoo.com>
2016-02-04 19:11:15 +0500

    add the mail and message in model and view

--- alfateh_fleet_management/route.py
+++ alfateh_fleet_management/route.py
@@ -5,6 +5,7 @@ from openerp import models, fields, api
 # Route Management
 class route_management(models.Model):
 	_name = 'route.management'
+	_inherit = ['mail.thread', 'ir.needaction_mixin']
 	route_defination = fields.Char('Route Defination')
 	route_time = fields.Float('Time(hours)')
 	route_distance = fields.Float('Distance(km)')
