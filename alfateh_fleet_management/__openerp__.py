f714c37b9ab79ed1b35bcd6afa7344bd7417d14d
ehtishamfaisal <ehtishamfaisal@gmail.com>
2016-02-04 16:47:19 +0500

    Brief Description of Module

--- alfateh_fleet_management/__openerp__.py
+++ alfateh_fleet_management/__openerp__.py
@@ -1,34 +1,23 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "alfateh_fleet_management",
+    'name': "Alfateh Fleet Management",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Purpose of this module is to manage Trip Management and Cost
+        of different Routes. It also include Schedule Management, Card Management, 
+        Logs and Reporting """,
 
     'description': """
-        Long description of module's purpose
+        
     """,
 
-    'author': "Your Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Uncategorized',
+    'author': "OxenLab",
+    'website': "http://www.oxenlab.com",
+    'category': 'aaps',
     'version': '0.1',
-
-    # any module necessary for this one to work correctly
     'depends': ['base','fleet'],
-
-    # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo.xml',
     ],
 }
