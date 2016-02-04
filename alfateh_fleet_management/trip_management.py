afe98bf6e449b0db08558dd29eb27361c7d850f8
ehtishamfaisal <ehtishamfaisal@gmail.com>
2016-02-04 19:38:25 +0500

    Trip Management fields added

--- alfateh_fleet_management/trip_management.py
+++ alfateh_fleet_management/trip_management.py
@@ -7,10 +7,19 @@ class trip_management(models.Model):
 	_name = 'trip.management'
 	_inherit = ['mail.thread', 'ir.needaction_mixin']
 	name = fields.Char('Name')
-	route_distance = fields.Float('KM')
-	trip_cost = fields.Float('Trip Total Cost')
-	projected_trip_cost = fields.Float('Projected Cost')
-	actual_trip_cost = fields.Float('Actual Cost')
 	vehicle = fields.Many2one('fleet.vehicle','Vehicle')
-
-
+	route = fields.Many2one('route.management','Route')
+	route_distance = fields.Float('Route Distance')
+	projected_trip_time = fields.Float('Projected Time')
+	projected_trip_fuel = fields.Float('Projected Fuel')
+	projected_trip_cost = fields.Float('Projected Cost')	
+	actual_trip_time = fields.Float('Actual Time')
+	actual_trip_fuel = fields.Float('Actual Fuel')
+	actual_trip_cost = fields.Float('Actual Cost')
+	trip_cost = fields.Float('Trip Total Cost')
+	@api.onchange('route')
+	def onchange_route_field(self):
+		self.route_distance = self.route.route_distance
+		self.projected_trip_time = self.route.route_time
+		self.projected_trip_fuel = self.route.route_fuel
+		self.projected_trip_cost = self.route.route_cost
