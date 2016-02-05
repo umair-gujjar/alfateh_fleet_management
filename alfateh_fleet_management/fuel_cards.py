8b3fc5c5426f7ab786b68991b5ca8fe24ebfc44f
umair-gujjar <engrumair_sabir@yahoo.com>
2016-02-05 23:45:28 +0500

    add graph view

--- alfateh_fleet_management/fuel_cards.py
+++ alfateh_fleet_management/fuel_cards.py
@@ -2,13 +2,14 @@
 
 from openerp import models, fields, api
 
-# Route Management
+# Fuel card Management
 class fuelcards_management(models.Model):
 	_name = 'fuelcard.management'
 	_inherit = ['mail.thread', 'ir.needaction_mixin']
-	card_name = fields.Char('Card Name')
+	card_name = fields.Char('Card Number')
 	card_company = fields.Char('Company')
 	card_limit = fields.Float('Limit( in liters)')
 	card_issue_data = fields.Date('Issue Date')
 	card_expiry_data = fields.Date('Expiry Date')
+	card_description = fields.Text('Description')
 
--- alfateh_fleet_management/route.py
+++ alfateh_fleet_management/route.py
@@ -6,10 +6,15 @@ from openerp import models, fields, api
 class route_management(models.Model):
 	_name = 'route.management'
 	_inherit = ['mail.thread', 'ir.needaction_mixin']
-	name = fields.Char('Name')
+	name = fields.Char('Name',readonly=True)
 	route_defination = fields.Char('Route Defination')
 	route_time = fields.Float('Time(hours)')
 	route_distance = fields.Float('Distance(km)')
 	route_fuel = fields.Float('Fuel(liters)')
 	route_cost = fields.Float('Cost')
+	route_description = fields.Text('Description')
 
+	def create(self, cr, uid, vals, context=None):
+		sequence=self.pool.get('ir.sequence').get(cr, uid, 'route.management')
+		vals['name']=sequence
+		return super(route_management, self).create(cr, uid, vals, context=context)
