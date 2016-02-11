03aae3f3235ffd47e81e5fbcdfd17e28ca942bfd
umair-gujjar <engrumair_sabir@yahoo.com>
2016-02-11 22:19:11 +0500

    add sql constraint on route name

--- alfateh_fleet_management/route.py
+++ alfateh_fleet_management/route.py
@@ -5,7 +5,7 @@ from openerp import models, fields, api
 class route_management(models.Model):
 	_name = 'route.management'
 	_inherit = ['mail.thread', 'ir.needaction_mixin']
-	name = fields.Char('Name')
+	name = fields.Char('Name' ,readonly= True)
 	route_defination = fields.Many2many('route.locations',string='Route Defination')
 	route_time = fields.Float('Time(hours)')
 	route_distance = fields.Float('Distance(km)')
@@ -14,6 +14,13 @@ class route_management(models.Model):
 	route_other_cost = fields.Float('Other Cost')
 	route_total_cost = fields.Float('Total Cost')
 	route_description = fields.Text('Description')
+
+
+	_sql_constraints = [
+    ('name_unique', 'unique(name)', 'This Route already exists!')
+	]
+
+
 	@api.onchange('route_defination')
 	def route_defination_name_get(self):
 		let_say_name = self.name
