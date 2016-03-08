ae41b37fffc1043d731137192a2a4aae91036eb5
ehtishamfaisal <ehtishamfaisal@gmail.com>
2016-03-08 19:30:26 +0500

    check list file

---
+++ alfateh_fleet_management/checklist.py
@@ -0,0 +1,55 @@
+from openerp import models, fields, api
+
+class check_list(models.Model):
+	_name = 'check_list'
+	name = fields.Char('Name')
+	check_list_id= fields.One2many('check_list_fields','check_list_fields_id')
+
+
+
+class check_list_fields(models.Model):
+	_name = 'check_list_fields'
+	name = fields.Char('Name')
+	daily_vehicle_check = fields.Char('Name')
+	status_yes = fields.Boolean('Yes')
+	status_no = fields.Boolean('No')
+	descp_of_defect = fields.Char('Description of Defect')
+	defect_corrected = fields.Char('Defect Corrected')
+	check_list_fields_id = fields.Many2one('check_list','Check List')
+
+
+class select_check_list(models.Model):
+	_name = 'select_check_list'
+	name = fields.Many2one('check_list','Name')
+	select_check_list_id = fields.One2many('select_check_list_fields','select_check_list_fields_id')
+	@api.multi
+	def action_create_test(self):
+		self.select_check_list_id = self._prepare_inspection_lines(self.name)
+		return True
+	@api.multi
+	def _prepare_inspection_lines(self, test, force_fill=False):
+		new_data = []
+		for line in test.check_list_id:
+			data = self._prepare_inspection_line(test, line, fill=force_fill)
+			new_data.append((0, 0, data))
+		return new_data
+	@api.multi
+	def _prepare_inspection_line(self, test, line, fill=None):
+		data = {
+			'daily_vehicle_check': line.daily_vehicle_check,
+			'status_yes': line.status_yes,
+			'status_no': line.status_no,
+			'descp_of_defect': line.descp_of_defect,
+			'defect_corrected': line.defect_corrected,
+		}
+		return data
+
+class select_check_list_fields(models.Model):
+	_name = 'select_check_list_fields'
+	name = fields.Char('Name')
+	daily_vehicle_check = fields.Char('Name')
+	status_yes = fields.Boolean('Yes')
+	status_no = fields.Boolean('No')
+	descp_of_defect = fields.Char('Description of Defect')
+	defect_corrected = fields.Char('Defect Corrected')
+	select_check_list_fields_id = fields.Many2one('select_check_list','Check List')
