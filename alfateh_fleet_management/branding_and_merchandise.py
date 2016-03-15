5ab1885d4a65380b1c68574e9098e3c77e59321e
umair-gujjar <engrumair_sabir@yahoo.com>
2016-03-15 16:23:17 +0500

    add assesment form

--- alfateh_fleet_management/branding_and_merchandise.py
+++ alfateh_fleet_management/branding_and_merchandise.py
@@ -29,8 +29,7 @@ class branding_merchandise(models.Model):
 	designation = fields.Char('Designation')
 	email_address = fields.Char('Email Address')	
 	representative_name = fields.Char('Name of Representative')
-	#company_name = fields.Char('Name of Company')
-	company_name = fields.Many2one('res.partner','Name of Company',required=True)
+	name_of_company= fields.Many2one('res.partner','Name of Company',required=True)
 	registered_address = fields.Char('Registered Address')
 	website = fields.Char('Website ')
 	ntn_num = fields.Char('NTN # ')
@@ -46,49 +45,121 @@ class branding_merchandise(models.Model):
 	company_type =  fields.Char()
 	previous_contract =  fields.Selection([('yes', 'Yes'),
 		('no', 'No')], string="previous Contract with our Company(if Any)")
+	assesment_list_name = fields.Many2one('assesment_list','Select Assesment List')
+	assesmnet_date = fields.Date('Date')
+	company_name = fields.Char('Company Name')
+	department = fields.Char('Department')
+	sa_num = fields.Char('SA #')
+	select_assesment_list_id = fields.One2many('select_assesment_list_fields','select_assesment_list_fields_id')
+	@api.multi
+	def action_create_assesment_list(self):
+		self.select_assesment_list_id = self._prepare_assesment_list_lines(self.assesment_list_name)
+		return True
+	@api.multi
+	def _prepare_assesment_list_lines(self, assesment, force_fill=False):
+		new_data = []
+		for line in assesment.assesment_list_id:
+			data = self._prepare_assesment_list_line(assesment, line, fill=force_fill)
+			new_data.append((0, 0, data))
+		return new_data
+	@api.multi
+	def _prepare_assesment_list_line(self, assesment, line, fill=None):
+		data = {
+			'assesment_check': line.assesment_check,
+			'factor': line.factor,
+			'grade': line.grade,
+		}
+		return data
 
 
 	state = fields.Selection([('application', 'Application'),
-		('assesment', 'Assessment'),
-		('finished', 'Done'),],default='application')
+		('assesment', 'Assessment'),],default='application')
 
 
-	@api.onchange('company_name')
-	def onchange_route_field(self):
-		self.ntn_num = self.company_name.x_ntn_num
-		self.stn_num = self.company_name.x_stn_num
-		self.website = self.company_name.website
-		self.company_type = self.company_name.x_company_type
-		print self.company_name.name
+	@api.onchange('name_of_company')
+	def onchange_assesment_form_field(self):
+		self.ntn_num = self.name_of_company.x_ntn_num
+		self.stn_num = self.name_of_company.x_stn_num
+		self.website = self.name_of_company.website
+		self.company_type = self.name_of_company.x_company_type
+		self.company_name = self.name_of_company.name
+		print self.name_of_company.name
 
 
 	@api.multi
-	def concept_progressbar(self):
+	def go_to_assesment(self):
 		self.ensure_one()
 		self.write({
 			'state': 'assesment',
 			})
 
 	@api.multi
-	def started_progressbar(self):
+	def go_to_application(self):
 		self.ensure_one()
 		self.write({
-			'state': 'started'
+			'state': 'application'
 			})
 
-	@api.multi
-	def progress_progressbar(self):
-		self.ensure_one()
-		self.write({
-			'state': 'progress'
-			})
 
-	@api.multi
-	def done_progressbar(self):
-		self.ensure_one()
-		self.write({
-			'state': 'finished',
-			})
+class assesment_list(models.Model):
+	_name = 'assesment_list'
+	name = fields.Char('Name')
+	assesment_list_id= fields.One2many('assesment_list_fields','assesment_list_fields_id')
+
+
+
+class assesment_list_fields(models.Model):
+	_name = 'assesment_list_fields'
+	name = fields.Char('Name')
+	assesment_check = fields.Char('Name')
+	factor = fields.Float('Factor')
+	grade =  fields.Selection([('1', 'Poor/No'),
+		('2', 'Average'),
+		('3', 'Good'),
+		('4', 'Excellent/Yes')], string="Grade")
+	
+	assesment_list_fields_id = fields.Many2one('assesment_list','Check List')
+
+
+
+#class select_assesment_list(models.Model):
+	#_name = 'select_assesment_list'
+	#name = fields.Many2one('assesment_list','Select Assesment List')
+	#assesmnet_date = fields.Date('Date')
+	#company_name = fields.Char('Company Name')
+	#department = fields.Char('Department')
+	#sa_num = fields.Char('SA #')
+	#select_assesment_list_id = fields.One2many('select_assesment_list_fields','select_assesment_list_fields_id')
+	#@api.multi
+	#def action_create_assesment_list(self):
+		#self.select_assesment_list_id = self._prepare_assesment_list_lines(self.name)
+		#return True
+	#@api.multi
+	#def _prepare_assesment_list_lines(self, check, force_fill=False):
+		#new_data = []
+		#for line in check.check_list_id:
+			#data = self._prepare_assesment_list_line(check, line, fill=force_fill)
+			#new_data.append((0, 0, data))
+		#return new_data
+	#@api.multi
+	#def _prepare_assesment_list_line(self, check, line, fill=None):
+		#data = {
+			#'assesment_check': line.assesment_check,
+			#'factor': line.factor,
+			#'grade': line.grade,
+		#}
+		#return data
+
+class select_assesment_list_fields(models.Model):
+	_name = 'select_assesment_list_fields'
+	name = fields.Char('Name')
+	assesment_check = fields.Char('Name')
+	factor = fields.Float('Factor')
+	grade =  fields.Selection([('1', 'Poor/No'),
+		('2', 'Average'),
+		('3', 'Good'),
+		('4', 'Excellent/Yes')], string="Grade")
+	select_assesment_list_fields_id = fields.Many2one('branding.management','Check List')
 
 	
 
