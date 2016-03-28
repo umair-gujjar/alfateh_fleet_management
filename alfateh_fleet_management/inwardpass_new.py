aab2462dde2e4081b5fb19bc75a9a058a56f5fc0
ehtishamfaisal <ehtishamfaisal@gmail.com>
2016-03-28 11:03:35 +0500

    sequence and name issue resolved

--- alfateh_fleet_management/inwardpass_new.py
+++ alfateh_fleet_management/inwardpass_new.py
@@ -7,10 +7,7 @@ class outwardpass(osv.Model):
 	'name': fields.char('Name',readonly=True),
 	'date' : fields.date('Date', ),
 	'invoice_ref' : fields.char('Invoice Ref #', size=32),
-	'gin_shop' : fields.char('GIN #',size=32 ),
-	'gin_mkt_pl' : fields.char('GIN #',size=32 ),
-	'gin_gen' : fields.char('GIN #',size=32 ),
-	'gin_ret' : fields.char('GIN #',size=32 ),
+	'gin' : fields.char('GIN #',size=32 ),
 	'bilty' : fields.char('Bilty ',size=32),
 	'time_out' : fields.datetime('Time Out', ),
 	'vehicle' : fields.char('Vehicle',size=32),
@@ -31,10 +28,7 @@ class outwardpass(osv.Model):
 	'out_driver' : fields.many2one('res.partner','Driver',domain="['|',('customer','!=',True),('employee','=',True)]"),
 	'out_date' : fields.date('Date', ),
 	'out_nature' : fields.boolean('Return or Rejection'),
-	'out_gon_shop' : fields.char('GON #',),
-	'out_gon_mkt_pl' : fields.char('GON #',),
-	'out_gon_gen' : fields.char('GON #',),
-	'out_gon_ret' : fields.char('GON #',),
+	'out_gon' : fields.char('GON #',),
 	'out_sron' : fields.char('SRON Ref.',size=32),
 	'out_time_out' : fields.datetime('Time Out',),
 	'out_vehicle' : fields.char('Vehicle',size=32),
@@ -53,6 +47,7 @@ class outwardpass(osv.Model):
 	'out_inret_id' : fields.one2many('out_inret','out_inwardret_id',string='Details'),
 	'in_inshop_id' : fields.one2many('in_inshop','in_inwardshop_id',string='Details'),
 	'out_inshop_id' : fields.one2many('out_inshop','out_inwardshop_id',string='Details'),
+	'select_sequence' : fields.many2one('ir.sequence','Select Sequence',required=True,help="Please select the sequence."),
 	'workers': fields.char('No of Workers',size=32),
 	'lc_pc' : fields.selection([
             ('lc', 'LC'),
@@ -78,82 +73,14 @@ class outwardpass(osv.Model):
 
 	def create(self, cr, uid, vals, context=None):
 		sequence=self.pool.get('ir.sequence').get(cr, uid, 'outwardpass')
-		# gate in shop seq
-		seq_gin_shop = self.pool.get('ir.sequence').get(cr, uid, 'Shopinpass')
-		seq_gin_shop_test=self.pool.get('ir.sequence').search(cr, uid, [('code','=','Shopinpass')], context=context)
-		seq_gin_shop_res = self.pool.get('ir.sequence').browse(cr, uid, seq_gin_shop_test)
-		# gate OUT shop seq
 		seq_out_gon_shop = self.pool.get('ir.sequence').get(cr, uid, 'Shopoutpass')
-		seq_out_gon_shop_test=self.pool.get('ir.sequence').search(cr, uid, [('code','=','Shopoutpass')], context=context)
-		seq_out_gon_shop_res = self.pool.get('ir.sequence').browse(cr, uid, seq_out_gon_shop_test)
-		# gate in markeet seq
-		seq_gin_mkt_pl = self.pool.get('ir.sequence').get(cr, uid, 'Mktinpass')
-		seq_gin_mkt_pl_test=self.pool.get('ir.sequence').search(cr, uid, [('code','=','Mktinpass')], context=context)
-		seq_gin_mkt_pl_res = self.pool.get('ir.sequence').browse(cr, uid, seq_gin_mkt_pl_test)
-		# gate out markeet seq
-		seq_out_gon_mkt_pl = self.pool.get('ir.sequence').get(cr, uid, 'Mktoutpass')
-		seq_out_gon_mkt_pl_test=self.pool.get('ir.sequence').search(cr, uid, [('code','=','Mktoutpass')], context=context)
-		seq_out_gon_mkt_pl_res = self.pool.get('ir.sequence').browse(cr, uid, seq_out_gon_mkt_pl_test)
-		# gate in general seq
-		seq_gin_gen = self.pool.get('ir.sequence').get(cr, uid, 'Geninpass')
-		seq_gin_gen_test=self.pool.get('ir.sequence').search(cr, uid, [('code','=','Geninpass')], context=context)
-		seq_gin_gen_res = self.pool.get('ir.sequence').browse(cr, uid, seq_gin_gen_test)
-		# gate out general seq
-		seq_out_gon_gen = self.pool.get('ir.sequence').get(cr, uid, 'Genoutpass')
-		seq_out_gon_gen_test=self.pool.get('ir.sequence').search(cr, uid, [('code','=','Genoutpass')], context=context)
-		seq_out_gon_gen_res = self.pool.get('ir.sequence').browse(cr, uid, seq_out_gon_gen_test)
-		# gate in returnable seq
-		seq_gin_ret = self.pool.get('ir.sequence').get(cr, uid, 'Retinpass')
-		seq_gin_ret_test=self.pool.get('ir.sequence').search(cr, uid, [('code','=','Retinpass')], context=context)
-		seq_gin_ret_res = self.pool.get('ir.sequence').browse(cr, uid, seq_gin_ret_test)
-		# gate out returnable seq
-		seq_out_gon_ret = self.pool.get('ir.sequence').get(cr, uid, 'Retoutpass')
-		seq_out_gon_ret_test=self.pool.get('ir.sequence').search(cr, uid, [('code','=','Retoutpass')], context=context)
-		seq_out_gon_ret_res = self.pool.get('ir.sequence').browse(cr, uid, seq_out_gon_ret_test)
-
-
-		if vals['outward_Category'] != 'Shop':
-			res_in_shop = seq_gin_shop_res.number_next_actual - 1
-			res_out_shop = seq_out_gon_shop_res.number_next_actual - 1
-			seq_gin_shop_res.number_next_actual = res_in_shop
-			seq_out_gon_shop_res.number_next_actual = res_out_shop
-		if vals['outward_Category'] != 'Market_Purchase':
-			res_in_mkt_pl = seq_gin_mkt_pl_res.number_next_actual - 1
-			res_out_mkt_pl = seq_out_gon_mkt_pl_res.number_next_actual - 1
-			seq_gin_mkt_pl_res.number_next_actual = res_in_mkt_pl
-			seq_out_gon_mkt_pl_res.number_next_actual = res_out_mkt_pl
-		if vals['outward_Category'] != 'General':
-			res_in_gen = seq_gin_gen_res.number_next_actual - 1
-			res_out_gen = seq_out_gon_gen_res.number_next_actual - 1
-			seq_gin_gen_res.number_next_actual = res_in_gen
-			seq_out_gon_gen_res.number_next_actual = res_out_gen
-		if vals['outward_Category'] != 'Returnable':
-			res_in_ret = seq_gin_ret_res.number_next_actual - 1
-			res_out_ret = seq_out_gon_ret_res.number_next_actual - 1
-			seq_gin_ret_res.number_next_actual = res_in_ret
-			seq_out_gon_ret_res.number_next_actual = res_out_ret
-
 		vals['name']="Out_"+vals['outward_Category']+"_"+sequence
-
-		vals['gin_shop'] = "Inward-"+vals['outward_Category']+"-"+seq_gin_shop
-		vals['out_gon_shop'] = "Outward-"+vals['outward_Category']+"-"+seq_out_gon_shop
-		vals['gin_mkt_pl'] = "Inward-"+vals['outward_Category']+"-"+seq_gin_mkt_pl
-		vals['out_gon_mkt_pl'] = "Outward-"+vals['outward_Category']+"-"+seq_out_gon_mkt_pl
-		vals['gin_gen'] = "Inward-"+vals['outward_Category']+"-"+seq_gin_gen
-		vals['out_gon_gen'] = "Outward-"+vals['outward_Category']+"-"+seq_out_gon_gen
-		vals['gin_ret'] = "Inward-"+vals['outward_Category']+"-"+seq_gin_ret
-		vals['out_gon_ret'] = "Outward-"+vals['outward_Category']+"-"+seq_out_gon_ret
+		vals['out_gon'] = "Outward-"+vals['outward_Category']+"-"+seq_out_gon_shop
 
 		return super(outwardpass, self).create(cr, uid, vals, context=context)
 	_defaults = {
-				'gin_shop': lambda obj, cr, uid, context: '/',
-				'out_gon_shop': lambda obj, cr, uid, context: '/',
-				'gin_mkt_pl': lambda obj, cr, uid, context: '/',
-				'out_gon_mkt_pl': lambda obj, cr, uid, context: '/',
-				'gin_gen': lambda obj, cr, uid, context: '/',
-				'out_gon_gen': lambda obj, cr, uid, context: '/',
-				'gin_ret': lambda obj, cr, uid, context: '/',
-				'out_gon_ret': lambda obj, cr, uid, context: '/',
+				'gin': lambda obj, cr, uid, context: '/',
+				'out_gon': lambda obj, cr, uid, context: '/',
 
 				'time_out': lambda obj, cr, uid, context: datetime.now(),
 				'time_in': lambda obj, cr, uid, context: datetime.now(),
@@ -228,6 +155,7 @@ class inwardpass(osv.Model):
 	'out_outret_id' : fields.one2many('out_outret','out_outwardret_id',string='Details'),
 	'out_stock_location_id' : fields.many2one('stock.location','Dept'),
 	'out_document_ref' : fields.char('Document Ref #',size=32),
+	'select_sequence' : fields.many2one('ir.sequence','Select Sequence',required=True,help="Please select the sequence."),
 	'lc_pc' : fields.selection([
             ('lc', 'LC'),
             ('pc', 'PO'),
@@ -251,9 +179,9 @@ class inwardpass(osv.Model):
 
 	def create(self, cr, uid, vals, context=None):
 		sequence=self.pool.get('ir.sequence').get(cr, uid, 'inwardpass')
+		seq_gin_shop = self.pool.get('ir.sequence').get(cr, uid, 'Genoutpass')
 		vals['name']="In_"+vals['inward_Category']+"_"+sequence
-		vals['gin'] = sequence
-		vals['out_gon'] = sequence
+		vals['gin'] = "Inward-"+vals['inward_Category']+"-"+seq_gin_shop
 		return super(inwardpass, self).create(cr, uid, vals, context=context)
 	_defaults = {
 				'gin': lambda obj, cr, uid, context: '/',
@@ -385,9 +313,12 @@ class gate_pass_outwardpass_inherit(models.Model):
 	@api.one
 	def vehicle_enter(self):
 		self.write({'state': 'vehicle_enter'})
+
 	@api.one
 	def vehicle_process(self):
 		self.write({'state': 'vehicle_process'})
+		self.gin = self.select_sequence.prefix + str(self.select_sequence.number_next_actual)
+		self.select_sequence.number_next_actual = self.select_sequence.number_next_actual + 1
 	@api.one
 	def vehicle_exit(self):
 		self.write({'state': 'vehicle_exit'})
@@ -420,8 +351,22 @@ class gate_pass_outwardpass_inherit(models.Model):
 		self.out_driver = self.trip_management_field.driver_id
 
 	
-
-
+	@api.onchange('outward_Category')
+	def onchange_outward_Category_field(self):
+		sequence = self.env['ir.sequence']
+		if self.outward_Category:
+			if self.outward_Category == 'Shop':
+				search_seq = sequence.search([('name','=','outshopseq')])
+				self.select_sequence = search_seq
+			if self.outward_Category == 'Market_Purchase':
+				search_seq_market = sequence.search([('name','=','outmarketseq')])
+				self.select_sequence = search_seq_market
+			if self.outward_Category == 'General':
+				search_seq_gen = sequence.search([('name','=','outgeneralseq')])
+				self.select_sequence = search_seq_gen
+			if self.outward_Category == 'Returnable':
+				search_seq_ret = sequence.search([('name','=','outreturnableseq')])
+				self.select_sequence = search_seq_ret
 # For Write method code
 	@api.multi
 	def write(self, values):
@@ -458,6 +403,8 @@ class gate_pass_inwardpass_inherit(models.Model):
 	@api.one
 	def vehicle_process(self):
 		self.write({'state': 'vehicle_process'})
+		self.out_gon = self.select_sequence.prefix + str(self.select_sequence.number_next_actual)
+		self.select_sequence.number_next_actual = self.select_sequence.number_next_actual + 1
 	@api.one
 	def vehicle_exit(self):
 		self.write({'state': 'vehicle_exit'})
@@ -486,6 +433,19 @@ class gate_pass_inwardpass_inherit(models.Model):
 		self.gpi_odoo_meter = self.trip_management_field.vehicle.odometer
 		self.out_driver = self.trip_management_field.driver_id
 
+	@api.onchange('inward_Category')
+	def onchange_inward_Category_field(self):
+		sequence = self.env['ir.sequence']
+		if self.inward_Category:
+			if self.inward_Category == 'supplier':
+				search_seq = sequence.search([('name','=','insupseq')])
+				self.select_sequence = search_seq
+			if self.inward_Category == 'general':
+				search_seq_gen = sequence.search([('name','=','ingeneralseq')])
+				self.select_sequence = search_seq_gen
+			if self.inward_Category == 'returnable':
+				search_seq_ret = sequence.search([('name','=','inreturnableseq')])
+				self.select_sequence = search_seq_ret
 
 # For Write method code
 	@api.multi
